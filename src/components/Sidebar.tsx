20f9716b7a396f321d0c4aedcd5aede8ec5edcab
semil21 <m7semil@gmail.com>
2024-01-31 11:09:30 +0530

    dashboard

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { sidebarMenuItems } from "../enums/sidebar";
+function Sidebar() {
+  const [sidebarItems, setSidebarItems] = useState(
+    (Object.keys(sidebarMenuItems) as Array<keyof typeof sidebarMenuItems>).map(
+      (key) => {
+        return key;
+      }
+    )
+  );
+
+  // const consoleCheck = (val) => {
+  //   console.log(val, "ssa");
+  // };
+
+  return (
+    <>
+      <button
+        type="button"
+        className="text-gray-500 hover:text-gray-600"
+        data-hs-overlay="#docs-sidebar"
+        aria-controls="docs-sidebar"
+        aria-label="Toggle navigation"
+      >
+        <span className="sr-only">Toggle Navigation</span>
+        <svg
+          className="flex-shrink-0 w-4 h-4"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+          />
+        </svg>
+      </button>
+
+      <div
+        id="docs-sidebar"
+        className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div className="px-6">
+          <a
+            className="flex-none text-xl font-semibold dark:text-white"
+            href="#"
+            aria-label="Brand"
+          >
+            Brand
+          </a>
+        </div>
+        <nav
+          className="hs-accordion-group p-6 w-full flex flex-col flex-wrap"
+          data-hs-accordion-always-open
+        >
+          {sidebarItems.map((value) => (
+            <ul className="space-y-1.5 py-1">
+              <li>
+                <a
+                  className="flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:bg-gray-900 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  href="#"
+                >
+                  <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                  <polyline points="9 22 9 12 15 12 15 22" />
+
+                  {sidebarMenuItems[value]}
+                </a>
+              </li>
+            </ul>
+          ))}
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
---
+++ src/enums/sidebar.ts
@@ -0,0 +1,13 @@
+export enum sidebarMenuItems {
+  learn_Html = "learn html",
+  learn_css = "learn css",
+  essay_on_car = "essay on car",
+  learn_js = "learn js",
+  learn_reactjs = "learn reactjs",
+  axios_post = "axios post",
+  navigation_in_nextjs = "navigation in nextjs",
+  ssr_in_nextjs = "ssr in nextjs",
+  update_record_in_nodejs = "update record in nodejs",
+  mongoose_connect = "mongoose connect",
+  express_json = "express json",
+}
