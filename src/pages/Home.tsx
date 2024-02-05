bcf8b9e3926e58d153586893547efde5c01c6ea2
Meet4230 <mpatel@plusonex.com>
2024-02-05 15:17:28 +0530

    refector chat ui and added types

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,6 @@ const Home = () => {
   return (
     <>
       {isAuthenticated && <Navigate to={"/dashboard"} />}
-      <h1>Home</h1>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={() => loginWithRedirect()}
